ddf8267eb016a88660605889c3c1292e719a7724
liukaiyang <liukaiyang@souche.com>
2018-10-29 17:12:30 +0800

    update

---
+++ src/main/java/com/jedijava/home/controller/index/IndexController.java
@@ -0,0 +1,15 @@
+package com.jedijava.home.controller.index;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author liukaiyang
+ * @date 2018/10/29 16:02
+ */
+@Controller
+public class IndexController {
+//    @RequestMapping(value = {"/","index"})
+//    public String index() {
+//        return "index.html";
+//    }
+}
